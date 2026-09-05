981f04b8691a69758bee73d5e125bef4789ec666
Wei He <carl.he87@yahoo.com>
2026-09-05 07:30:34 +0200

    Add Unicode abbreviations for special characters
    for neovim configuration

---
+++ lua/unicode.lua
@@ -0,0 +1,45 @@
+vim.cmd([[
+iabbrev ;mu μ
+iabbrev ;alpha α
+iabbrev ;beta β
+iabbrev ;gamma γ
+iabbrev ;Delta Δ
+
+iabbrev ;deg °
+iabbrev ;pm ±
+iabbrev ;approx ≈
+iabbrev ;ne ≠
+iabbrev ;le ≤
+iabbrev ;ge ≥
+iabbrev ;times ×
+iabbrev ;dot ·
+iabbrev ;arr →
+iabbrev ;eq ⇌
+
+iabbrev ;s0 ⁰
+iabbrev ;s1 ¹
+iabbrev ;s2 ²
+iabbrev ;s3 ³
+iabbrev ;s4 ⁴
+iabbrev ;s5 ⁵
+iabbrev ;s6 ⁶
+iabbrev ;s7 ⁷
+iabbrev ;s8 ⁸
+iabbrev ;s9 ⁹
+iabbrev ;s- ⁻
+iabbrev ;s+ ⁺
+
+iabbrev ;b0 ₀
+iabbrev ;b1 ₁
+iabbrev ;b2 ₂
+iabbrev ;b3 ₃
+iabbrev ;b4 ₄
+iabbrev ;b5 ₅
+iabbrev ;b6 ₆
+iabbrev ;b7 ₇
+iabbrev ;b8 ₈
+iabbrev ;b9 ₉
+iabbrev ;bn ₙ
+iabbrev ;bm ₘ
+iabbrev ;bx ₓ
+]])
